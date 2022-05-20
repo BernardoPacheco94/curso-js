051e0d51e8631ec55a76ce362e1cee4300aed862
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-20 17:59:37 -0300

    condicionais

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`Velocidade registrada: ${vel} Km/h`)
+if(vel > 60){
+    console.log ('Você foi multado por excesso de velocidade')
+}
+console.log ('Dirija com segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'Russia'
+if(pais != 'Brasil'){
+    console.log(`Vivendo em ${pais} você é Estrangeiro`)
+}else{
+    console.log(`Vivendo em ${pais} você é Brasileiro`)
+}
