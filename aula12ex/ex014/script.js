3e41e334855af7631ff6d82025e5ef3a1018d428
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-23 18:15:09 -0300

    ajuste verificar horas

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,24 +2,38 @@ function carregar() {
 var msg = document.getElementById('msg')
 var img = document.getElementById('img')
 var quadro = document.getElementsByTagName('section')[0]
-var titulo = document.getElementById
-('titulo_principal')
+var titulo = document.getElementById('titulo_principal')
 var data = new Date()
 var hora = data.getHours()
 
 titulo.innerHTML = `Hora do dia`
 msg.innerHTML = `Agora são ${hora} horas`
 
-if(hora >= 6 && hora < 12 ){
+if(hora >=0 && hora <= 5){
+    document.body.style.backgroundColor = '#1C4271'
+    img.style.backgroundImage = `url('../img/noite.jpg')`
+    quadro.style.backgroundColor = '#060709'
+    document.body.style.color = '#99CDFD'
+
+
+
+
+}else if(hora >= 6 && hora < 12 ){
     document.body.style.backgroundColor = '#98B6D0'
     img.style.backgroundImage = `url('../img/manha.jpg')`
     quadro.style.backgroundColor = '#C1AE86'
     document.body.style.color = '#1B2113'
-} else if(hora <= 18){
+
+
+
+} else if(hora < 19){
     document.body.style.backgroundColor = '#D1B552'
     img.style.backgroundImage = `url('../img/tarde.jpg')`
     quadro.style.backgroundColor = '#93805F'
     document.body.style.color = '#3B200D'
+
+
+
 } else{
     document.body.style.backgroundColor = '#1C4271'
     img.style.backgroundImage = `url('../img/noite.jpg')`
