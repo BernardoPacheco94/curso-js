9105e07910022005a5e86980f7b7bf03f4ca1d07
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-24 17:46:29 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -13,52 +13,62 @@ function verificar() {
         var genero = ''
         var img = document.getElementById('img')
 
-        if(fsex[0].checked){
+        if (fsex[0].checked) {
             genero = 'Homem'
-            res.innerHTML = `Detectado: ${genero} <br> Completa ${idade} anos em ${ano}`
+            res.innerHTML = `Detectado: ${genero}<br>Completa ${idade} anos em ${ano}`
             res.style.textAlign = `center`
             img.style.width = `250px`
             img.style.height = `250px`
 
-            if(idade >= 0 && idade < 11){
+            if (idade >= 0 && idade < 11) {
                 img.style.backgroundImage = `url('img/crianca-homem.jpeg')`
                 img.style.backgroundSize = `250px`
-                
-               
-            }else if(idade <= 18){
+
+
+            } else if (idade <= 18) {
                 img.style.backgroundImage = `url('img/jovem-homem.jpeg')`
                 img.style.backgroundSize = `250px`
                 img.style.backgroundPosition = `bottom center`
-            }else if(idade < 50){
+
+
+            } else if (idade < 50) {
                 img.style.backgroundImage = `url('img/adulto-homem.jpeg')`
                 img.style.backgroundSize = `250px`
-            }else{
+
+
+            } else {
                 img.style.backgroundImage = `url('img/idoso-homem.jpeg')`
                 img.style.backgroundSize = `250px`
             }
-        }else{
+        } else {
             genero = 'Mulher'
-            res.innerHTML = `Detectado: ${genero} <br> Completa ${idade} anos em ${ano}`
+            res.innerHTML = `Detectado: ${genero}<br>Completa ${idade} anos em ${ano}`
             res.style.textAlign = `center`
             img.style.width = `250px`
             img.style.height = `250px`
-            if(idade >= 0 && idade < 11){
+            if (idade >= 0 && idade < 11) {
                 img.style.backgroundImage = `url('img/crianca-mulher.jpeg')`
                 img.style.backgroundSize = `250px`
                 img.style.backgroundPosition = `center right`
-            }else if(idade < 18){
+
+
+            } else if (idade < 18) {
                 img.style.backgroundImage = `url('img/jovem-mulher.jpeg')`
                 img.style.backgroundPosition = `center`
                 img.style.backgroundSize = `cover`
-            }else if(idade < 50){
+
+
+            } else if (idade < 50) {
                 img.style.backgroundImage = `url('img/adulto-mulher.jpeg')`
                 img.style.backgroundSize = `250px`
-            }else{
+
+
+            } else {
                 img.style.backgroundImage = `url('img/idoso-mulher.jpeg')`
                 img.style.backgroundSize = `250px`
                 img.style.backgroundPosition = `bottom`
             }
         }
-        
+
     }
 }
