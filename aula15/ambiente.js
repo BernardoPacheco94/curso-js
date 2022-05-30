c4de2dbbc3c490de68807dadb71438398cf053ba
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-30 09:15:11 -0300

    funções de vetores

---
+++ aula15/ambiente.js
@@ -0,0 +1,7 @@
+let num = [5,3,4]
+console.log (`Meu array está assim: ${num}`)
+num.push(8)
+console.log (`Meu novo está assim: ${num}`)
+console.log (`Verificando o tamanho do array: ${num.length}`)
+console.log (`Ordenando o array: ${num.sort()}`)
+
---
+++ aula15/vetoresnatela.js
@@ -0,0 +1,15 @@
+let vetor = [2, 5, 8, 1, 4, 6]
+
+for( var pos = 0; pos < vetor.length; pos++){
+    console.log(`Na posição ${pos} o valor é ${vetor[pos]}`)
+}
+console.log('Outro método:')
+for (var pos in vetor){
+    console.log(`Na posição ${pos} o valor é ${vetor[pos]}`)
+}
+
+console.log('')
+console.log('Buscando valores:')
+let indice = vetor.indexOf(8)
+console.log(`O valor 8 está na posição ${indice}: `)
+
