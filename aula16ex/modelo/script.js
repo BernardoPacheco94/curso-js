a23c6f5b1844c97e2a2ae13d966637f609d24241
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-05-30 17:44:45 -0300

    estilo

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -10,61 +10,61 @@ let media_num = document.querySelector('p#media_num')
 let btn_finalizar = document.querySelector('button#btn_finalizar')
 
 
-function adicionar(){
+function adicionar() {
     n = Number(num.value)
 
-    if(n == ""){
+    if (n == "") {
         alert("Informe um número para ser adicionado")
-    }else if(n<0 || n>100){
+    } else if (n < 0 || n > 100) {
         alert('Número digitado é inválido')
-    }else{
-    numeros.push(n)//Inserindo o número no array
+    } else {
+        numeros.push(n)//Inserindo o número no array
 
-    
-    let item = document.createElement ('option')//exibindo os numeros no select
-    item.text = `${n}`
-    select.appendChild(item)
+
+        let item = document.createElement('option')//exibindo os numeros no select
+        item.text = `${n}`
+        select.appendChild(item)
     }
-    
+
     num.value = ""
 }
 
-function maior(vetor){
+function maior(vetor) {
     let maior = vetor[0]
-    for (var pos in vetor){
-        if(maior <= vetor[pos]){
+    for (var pos in vetor) {
+        if (maior <= vetor[pos]) {
             maior = vetor[pos]
         }
     }
 
-    return maior    
+    return maior
 }
 
-function menor(vetor){
+function menor(vetor) {
     let menor = vetor[0]
 
-    for (var pos in vetor){
-        if(menor >= vetor[pos]){
+    for (var pos in vetor) {
+        if (menor >= vetor[pos]) {
             menor = vetor[pos]
         }
     }
 
-    return menor   
+    return menor
 }
 
-function soma (vetor){
+function soma(vetor) {
     let soma = 0
-    for (pos in vetor){
+    for (pos in vetor) {
         soma += vetor[pos]
     }
 
     return soma
 }
 
-function finalizar(){
+function finalizar() {
     total_num.innerHTML = `A lista possui ${numeros.length} elementos`
     maior_num.innerHTML = `O maior número é ${maior(numeros)}`
     menor_num.innerHTML = `O menor número é ${menor(numeros)}`
     soma_numtotal.innerHTML = `A soma dos números é: ${soma(numeros)}`
-    media_num.innerHTML = `A média dos números informados é: ${(soma(numeros)/numeros.length).toFixed(2)}`
+    media_num.innerHTML = `A média dos números informados é: ${(soma(numeros) / numeros.length).toFixed(2)}`
 }
