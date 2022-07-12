648428cbaf2e734bcfd2453328ccb6d6ac3d7f6a
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-07-12 17:56:50 -0300

    comit

--- extras/7-days/7-days-4/script.js
+++ extras/7-days/7-days-4/script.js
@@ -1,18 +1,20 @@
-let num = document.querySelector('#input_tentativa')
 let div_resultado = document.querySelector('#div_resultado')
+resposta = Math.floor(Math.random() * (10 - 0 + 1))
+contador = 0
 
 function verificar() {
-    resposta = Math.floor(Math.random() * (10 - 0 + 1))
-    
-    tentativa = 1
+    contador += 1
+    let num = document.querySelector('#input_tentativa')
 
-    alert(resposta)
-
-    while(resposta != num && tentativa <=3){
-        txt = document.createElement('p')
-        txt.text = 'Tente novamente'
-
-        div_resultado.appendChild(txt)
-        tentativa += 1
+    if (contador < 3) {
+        if (num.value == resposta) {
+            div_resultado.innerHTML = `<p>Você acertou! A resposta era ${resposta}</p>`
+        }
+        else {
+            div_resultado.innerHTML += `<p>Tente novamente</p>`
+        }
+    } else{
+        div_resultado.innerHTML = `Você errou, a resposta era ${resposta}`
     }
+
 }
