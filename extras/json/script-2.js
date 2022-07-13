3df96cbd33c1b73a288e0a866cf18c8b72ce3214
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-07-13 12:14:41 -0300

    comit

---
+++ extras/json/script-2.js
@@ -0,0 +1,9 @@
+let objeto = {
+    'nome': 'bernardo',
+    'idade': '',
+    'linguagens':['javascript','angular','python']
+}
+
+objeto.idade = 15
+objeto.endereco = 'avenida dores'//adicionando construtor
+console.log(objeto)
--- extras/json/script.js
+++ extras/json/script.js
@@ -1,30 +1,31 @@
-const objeto = [{// objeto javascript
+const objeto = [
+    {// objeto javascript
 
-    "nome": "bernardo",
-    "idade": 27,
-    "trabalhando": true,
-    "hobbies": [1, "zarabatana", false],
+        "nome": "bernardo",
+        "idade": 27,
+        "trabalhando": true,
+        "hobbies": [1, "zarabatana", false],
 
-    "detalhes_trabalho": {
-        "empresa": "sistemas e informacao",
-        "cargo": "suporte"
+        "detalhes_trabalho": {
+            "empresa": "sistemas e informacao",
+            "cargo": "suporte"
+        },
+
+        "estado_civil": "bauduco"
     },
+    {
+        "nome": "gabriel",
+        "idade": 2,
+        "trabalhando": false,
+        "hobbies": ["beber", "fumar"],
 
-    "estado_civil": "bauduco"
-},
-{
-    "nome": "gabriel",
-    "idade": 2,
-    "trabalhando": false,
-    "hobbies": ["beber", "fumar"],
+        "detalhes_trabalho": {
+            "empresa": false,
+            "cargo": false
+        },
 
-    "detalhes_trabalho": {
-        "empresa": false,
-        "cargo": false
-    },
-
-    "estado_civil": "loco"
-}
+        "estado_civil": "loco"
+    }
 ]
 
 //Converte meu objeto js em json
@@ -37,7 +38,31 @@ console.log(obj)
 
 //o map percorre o array
 obj.map((pessoa) => {//função executada para cada objeto obj no array
-    console.log(pessoa.nome+' '+pessoa.idade)
+    console.log(pessoa.nome + ' ' + pessoa.idade)
 })
+
+objeto.push({ // adicionei elemnto no array obj
+    "nome": "edglayson",
+        "idade": 200,
+        "trabalhando": true,
+        "hobbies": ["futebol"],
+
+        "detalhes_trabalho": {
+            "empresa": false,
+            "cargo": false
+        },
+
+        "estado_civil": "casado"
+})
+
+console.log(objeto)
+
+console.log(objeto[2].nome)
+
+objeto[2].nome = 'aroldo' //alterando propriedade do obj pelo indice
+console.log(objeto[2].nome)
+console.log(objeto[2])
+
+
 
 
