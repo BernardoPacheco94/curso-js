96a7a3fdcb20f1b13c584b3240fd917e0cd8bc8e
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-07-13 16:28:01 -0300

    refatoracao

--- extras/7-days/7-days-5/script.js
+++ extras/7-days/7-days-5/script.js
@@ -1,34 +1,35 @@
-let frutas = [];
-let laticinios = [];
-let doces = [];
-let congelados = [];
-let comida = "";
-let categoria = "";
-
-let adicionarMais = "sim";  // valor inicial como "sim", porque a primeira vez sempre vai entrar no while
-while(adicionarMais != "não"){
-    adicionarMais = prompt("Você deseja adicionar uma comida na lista de compras? Responda 'sim' ou 'não'.");
-    while (adicionarMais != "sim" && adicionarMais != "não") {  //enquanto o texto lido for diferente de "sim" e de "não", exibir que não foi reconhecido e perguntar novamente
-	alert(`Operação não reconhecida!`);
-        adicionarMais = prompt("Você deseja adicionar uma comida na lista de compras? Responda 'sim' ou 'não'.");
-    }
-	
-    if (adicionarMais === "não"){  //se o texto lido for "não", sair do while
-        break;
-    }
-	
-    comida = prompt("Qual comida você deseja inserir?");
-    categoria = prompt("Em qual categoria essa comida se encaixa: 'frutas', 'laticínios', 'doces' ou 'congelados'?");
-    if(categoria === 'frutas'){
-        frutas.push(comida);
-    } else if (categoria === 'laticínios'){
-        laticinios.push(comida);
-    } else if (categoria === 'doces'){
-        doces.push(comida);
-    } else if (categoria === 'congelados'){
-        congelados.push(comida);
-    } else {
-        alert("Essa categoria não foi pré-definida.")
-    }
+let lista_compras = {
+    'laticineos': [],
+    'carnes': [],
+    'frutas': [],
+    'verduras': [],
+    'graos': []
 }
-alert(`Lista de compras:\n  Frutas: ${frutas}\n  Laticínios: ${laticinios}\n  Doces: ${doces}\n  Congelados: ${congelados}`);
+
+adc_comida = 's'
+
+function iniciar() {
+    while (adc_comida.toLowerCase() == 's') {
+        adc_comida = prompt('Deseja adicionar comida a sua lista de compras? (s/n)')
+
+        if (adc_comida.toLowerCase() == 's') {//se a resposta for s
+            comida = prompt('Informe qual seria a comida:')//solicita comida
+            categoria = prompt(`Em qual categoria ${comida} se encaixa? ${JSON.stringify(Object.keys(lista_compras))}`)//solicita categoria
+
+
+            try {//tenta incluir nas categorias existentes
+                lista_compras[`${categoria}`].push(comida)
+            } catch (error) {//se nao conseguir, verifica se quer criar
+                criar_categoria = prompt(`categoria não encontrada, deseja criar categoria "${categoria}"? (s/n)`)
+                if (criar_categoria.toLowerCase() == 's') {
+                    lista_compras[categoria.toLowerCase()] = [comida]
+                }
+            }
+        }
+
+        alert(JSON.stringify(lista_compras))
+
+    }
+    alert('Lista encerrada!')
+
+}
--- extras/json/script-2.js
+++ extras/json/script-2.js
@@ -1,9 +1,24 @@
-let objeto = {
-    'nome': 'bernardo',
-    'idade': '',
-    'linguagens':['javascript','angular','python']
+let lista_compras = {
+    'laticineos' : ['leite', 'queijo'],
+    'frutas' : []
 }
 
-objeto.idade = 15
-objeto.endereco = 'avenida dores'//adicionando construtor
-console.log(objeto)
+
+lista_compras.matinais = ['pao','presunto']
+lista_compras.frutas.push('maçã', 'banuina')
+
+categorias = Object.keys(lista_compras)//verifica chaves do objeto
+
+function exibeCategorias(array){//formata exibição das categorias
+    exibicao = ''
+    for (i=0; i<array.length; i++){
+        exibicao += categorias[i]+'\n'
+    }
+    return exibicao
+}
+
+console.log(exibeCategorias(categorias))//exibindo categorias formadas
+
+console.log(Object.values(lista_compras))
+
+
