63f1f364258ce3bd0c4f1be14befb544b3db8b47
Bernardo Pacheco <102324672+BernardoPacheco94@users.noreply.github.com>
2022-07-19 14:53:19 -0300

    finalizado

--- extras/7-days/7-days-6/script.js
+++ extras/7-days/7-days-6/script.js
@@ -12,8 +12,8 @@ let lista_compras = {
     'graos': []
 }
 
-function carregar(){
-    //Cria as entradas de dados no HTML...
+//Cria as entradas de dados no HTML...
+function carregar() {
     div_radio.innerHTML = ''
     array_categorias = Object.keys(lista_compras)
 
@@ -23,10 +23,10 @@ function carregar(){
         input.setAttribute("name", "lista_compras")
         input.setAttribute("id", `${item}_input`)
         input.setAttribute("value", `${item}`)
-        
+
         label = document.createElement('label')
         label.setAttribute("for", `${item}_input`)
-        
+
 
         label.innerHTML = ` <br>${item}`
         div_radio.appendChild(label)
@@ -34,27 +34,25 @@ function carregar(){
     })
 }
 
-
 //Inclusao dos produtos nas listas
-function iniciar() {
+function adcComida() {
     radios = document.getElementsByName('lista_compras')
     pegaRadio()
 
     input_comida = document.getElementById('input_comida')
     lista_compras[`${pegaRadio()}`].push(input_comida.value)
+    input_comida.value = ``
     exibeListas()
 }
 
 //Retorna o rádio que foi selecionado
-function pegaRadio (){
-    for (radio of radios){
-        if (radio.checked){
+function pegaRadio() {
+    for (radio of radios) {
+        if (radio.checked) {
             return radio.value
         }
     }
 }
-
-
 
 function exibeListas() {
     div.innerHTML = ''
@@ -63,6 +61,8 @@ function exibeListas() {
 
     for (i = 0; i < array_categorias.length; i++) {//percorre as chaves da lista de compras
         select = document.createElement('select')
+        select.setAttribute("id", `select_${array_categorias[i]}`)
+        select.setAttribute("name", `${array_categorias[i]}`)
         select.setAttribute("size", "10")
         array_produtos = Object.values(lista_compras[`${array_categorias[i]}`])//coloca os valores de cada indice num array
 
@@ -75,22 +75,23 @@ function exibeListas() {
 
 
         btn_remover_selecionado = document.createElement('button')
-        btn_remover_selecionado.setAttribute("onclick", "removerItem()")
+        btn_remover_selecionado.setAttribute("onclick", `removerItem(select_${array_categorias[i]})`)
         btn_remover_selecionado.innerText = 'Remover Item'
         div.innerHTML += `
         <h1>${array_categorias[i]}</h1> <br>
         ${select.outerHTML} <br>
-        ${btn_remover_selecionado.outerHTML}
-        `
-
+        ${btn_remover_selecionado.outerHTML}`
     }
 }
 
-function novaCategoria(){
+function novaCategoria() {
     lista_compras[`${input_comida.value}`] = []
     carregar()
 }
 
-function removerItem(){
-    alert('IHUUUUUUUUU')
+function removerItem(select) {
+    pos = select.selectedIndex
+    lista_compras[`${select.name}`].splice(pos, 1)
+    alert('Item removido!')
+    exibeListas()
 }
